1e6d41bd36a08fdac73f0dc6f0fdcd10e0956559
DeForestt <deforesttthompson@gmail.com>
2022-07-18 15:55:38 -0400

    Build and run localy and then run

--- src/routes/box/controllers/run.ts
+++ src/routes/box/controllers/run.ts
@@ -1,30 +1,39 @@
 import express from "express";
-import { exec } from "child_process";
+import { execSync } from "child_process";
 import fs from "fs";
+import path from "path";
+import {wwwroot} from "../constants"
+import {randomUUID} from "crypto"
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 interface RunRSP {
-    milis: number;
     output: string;
+    milis: number;
 }
 
 interface File {
+    id: number;
     name: string;
-    text: string;
+    data: string;
 };
 
-const runCode = async (input : string) => {
-    let output = '';
-
-    exec("cd")
-};
+const runCode = async (input : string) : Promise<string> => {
+    const boxID = randomUUID();
+    const boxPath = path.join(wwwroot, 'Boxes', boxID);
+    execSync(`(aflat make ${boxPath})`);
+    fs.writeFileSync(path.join(boxPath, 'src', 'main.af'), input);
+    execSync(`(cd ${boxPath}; aflat run > './output.txt')`);
+    let output : Promise<string> = fs.promises.readFile(path.join(boxPath, 'output.txt'), 'utf-8');
+    fs.rm(boxPath, {recursive: true} ,err => { if (err) return console.log(err)});
+    return output;
+};    
 
 const Run = async (_req : express.Request) : Promise<RunRSP> => {
     const file : File = _req.body;
-    // save the body to a file...
-
+    const output: string = await runCode(file.data);
     return {
-        milis: 0,
-        output: 'The box has been run'
+        output: output,
+        milis: 0
     }
 };
 
