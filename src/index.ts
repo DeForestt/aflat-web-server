a59da5862140fdab6e3af9f0479cacc7c1aabb1c
DeForestt <deforesttthompson@gmail.com>
2022-09-02 23:11:21 -0400

    https

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,29 @@ import https from "https";
 import fs from "fs";
 import Router from "./routes";
 
+interface HTTPOptions {
+  ca: Buffer | undefined;
+  key: Buffer | undefined;
+  cert: Buffer | undefined;
+}
+
 const PORT = process.env.PORT || 8000;
 const HTTPS_PORT = process.env.HTTPS_PORT || 8443;
 
 const CERT = process.env.AFLAT_SERVER_CERT
 const KEY = process.env.AFLAT_SERVER_KEY
+const CA = process.env.AFLAT_SERVER_CA
 
 // read the certificate and key
+
+const options: HTTPOptions = {
+  ca: CA ? fs.readFileSync(CA) : undefined,
+  key: KEY ? fs.readFileSync(KEY) : undefined,
+  cert: CERT ? fs.readFileSync(CERT) : undefined
+};
+
+console.log(options);
+
 const cert = CERT ? fs.readFileSync(CERT) : undefined;
 const key = KEY ? fs.readFileSync(KEY) : undefined;
 
@@ -45,7 +61,7 @@ app.listen(PORT, () => {
   });
 
 
-let httpsServer = https.createServer({cert, key}, app);
+let httpsServer = https.createServer(options, app);
 httpsServer.listen(HTTPS_PORT, () => {
     console.log("Server is running on port", HTTPS_PORT);
     console.log(`Find server docs at https://localhost:${HTTPS_PORT}/docs`);
