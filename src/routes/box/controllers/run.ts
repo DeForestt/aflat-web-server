21f24b19a277151ba41af70adaaa02ed4235466a
DeForestt <deforesttthompson@gmail.com>
2022-09-04 23:18:16 -0400

    new scheam

--- src/routes/box/controllers/run.ts
+++ src/routes/box/controllers/run.ts
@@ -4,24 +4,42 @@ import fs from "fs";
 import path from "path";
 import {wwwroot} from "../constants"
 import {randomUUID} from "crypto"
-import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 interface RunRSP {
     output: string;
     milis: number;
 }
 
+export interface CodeFile {
+    name: string
+    content: string
+}
+
+export interface AflatProject {
+    main: CodeFile;
+    test?: CodeFile;
+    modules ?: [CodeFile];
+}
+
 interface File {
     id: number;
-    name: string;
-    data: string;
+    data: AflatProject;
 };
 
-const runCode = async (input : string) : Promise<string> => {
+const runCode = async (project : AflatProject) : Promise<string> => {
     const boxID = randomUUID();
     const boxPath = path.join(wwwroot, 'Boxes', boxID);
     execSync(`(aflat make ${boxPath})`);
-    fs.writeFileSync(path.join(boxPath, 'src', 'main.af'), input);
+    fs.writeFileSync(path.join(boxPath, 'src', 'main.af'), project.main.content);
+    if (project.test) {
+        fs.writeFileSync(path.join(boxPath, 'test', 'test.af'), project.test.content);
+    }
+    if (project.modules) {
+        project.modules.forEach((module) => {
+            fs.writeFileSync(path.join(boxPath, 'src', module.name + '.af'), module.content);
+        });
+    }
+
     execSync(`(cd ${boxPath}; aflat run > './output.txt')`);
     let output : Promise<string> = fs.promises.readFile(path.join(boxPath, 'output.txt'), 'utf-8');
     fs.rm(boxPath, {recursive: true} ,err => { if (err) return console.log(err)});
