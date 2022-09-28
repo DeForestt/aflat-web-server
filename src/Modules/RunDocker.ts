8debec8e8f7467420054606267d3ef781a99eac0
deforestt <deforesttthompson@gmail.com>
2022-09-28 04:04:28 -0400

    added building aflat to docker workflow

--- src/Modules/RunDocker.ts
+++ src/Modules/RunDocker.ts
@@ -1,5 +1,5 @@
 import { exec, execSync } from "child_process";
 
 export function runDockerContainer (path : string, command: string) {
-    return execSync(`(cd ${path} && docker run --rm -v $(pwd):/app -w /app aflat:latest ${command} < stdin.txt) 2> null`);
+    return execSync(`(cd ${path} && docker run --rm -v $(pwd):/app -w /app aflat:latest ${command} < stdin.txt) 2> ${path}/null`);
 }
