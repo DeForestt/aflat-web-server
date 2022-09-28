5f4fd8eeacddf6210c006bb56d4130cebf91a132
deforestt <deforesttthompson@gmail.com>
2022-09-28 04:42:27 -0400

    keep logs

--- src/Modules/RunDocker.ts
+++ src/Modules/RunDocker.ts
@@ -1,5 +1,5 @@
 import { exec, execSync } from "child_process";
 
 export function runDockerContainer (path : string, command: string) {
-    return execSync(`(cd ${path} && sudo docker run --rm -v $(pwd):/app -w /app aflat:latest ${command} < stdin.txt) >> ${path}/null`);
+    return execSync(`(cd ${path} && sudo docker run --rm -v $(pwd):/app -w /app aflat:latest ${command} < stdin.txt) 2> ${path}/null`);
 }
